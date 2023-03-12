ba79f1e229a9f7a142b989d37ca06de72fff3bd3
aravnanda2019 <aravnanda2019@gmail.com>
2023-03-12 18:40:43 +0530

    updating ./roboshop-shell

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,15 @@
+source common.sh
+
+print_head "Setup MongoDB Repository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head "Install MongoDB"
 yum install mongodb-org -y
+
+print_head "Enable MongoDB Service"
 systemctl enable mongod
+
+print_head "Start MongoDB Service"
 systemctl start mongod
 
-# update /etc/mongod.conf file from 127.0.0.1 with 0.0.0.0
+# Update /etc/mongod.conf file from 127.0.0.1 with 0.0.0.0
