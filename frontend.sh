0dfe7ef2fde5e02a24d30db674fbde42c1fcbdbf
aravnanda2019 <aravnanda2019@gmail.com>
2023-03-12 14:48:07 +0530

    Create Servers Script

--- frontend.sh
+++ frontend.sh
@@ -5,3 +5,6 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 systemctl enable nginx
 systemctl start nginx
+
+## Roboshop config is not copied
+## If any command is errored or failed, we need to stop the script
