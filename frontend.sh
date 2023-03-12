1429b9df50d0d544bc552d2a363af5b9e80cabc6
aravnanda2019 <aravnanda2019@gmail.com>
2023-03-12 18:27:06 +0530

    updating ./roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -11,20 +11,20 @@ yum install nginx -y &>>${log_file}
 print_head "Removing Old Content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 
-print_head "mDownloading Frontend Content"
+print_head "Downloading Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
 print_head "Extracting Downloaded Frontend"
 cd /usr/share/nginx/html &>>${log_file}
 unzip /tmp/frontend.zip &>>${log_file}
 
-print_head "mCopying Nginx Config for Roboshop"
+print_head "Copying Nginx Config for Roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
 print_head "Enable Nginx Server"
 systemctl enable nginx &>>${log_file}
 
-print_head "mRestarting Nginx Server"
+print_head "Restarting Nginx Server"
 systemctl restart nginx &>>${log_file}
 
 
