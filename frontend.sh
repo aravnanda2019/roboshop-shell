f216ba8ea82e9d7a86078ddcce605bc90868c879
aravnanda2019 <aravnanda2019@gmail.com>
2023-03-16 17:21:55 +0530

    updating ./roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,12 @@ source common.sh
 
 print_head "Installing Nginx"
 yum install nginx -y &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi  
+
 
 print_head "Removing Old Content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
