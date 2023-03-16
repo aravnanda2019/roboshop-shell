de3b4c48136a12e8903ee95799d4d31978a9ceb0
aravnanda2019 <aravnanda2019@gmail.com>
2023-03-16 14:13:20 +0530

    updating ./roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,47 +1,48 @@
 source common.sh
 
-print_head "Configure NodeJs Repo"
+print_head "configuring nodejs repo "
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
-print_head "Install NodeJS"
+print_head "Installaling nodejs "
 yum install nodejs -y &>>${log_file}
 
-print_head "Create Roboshop User"
+print_head "created new user Roboshop"
 useradd roboshop &>>${log_file}
 
-print_head "Create Application Directory"
+print_head "Created application Directory "
 mkdir /app &>>${log_file}
 
-print_head "Delete Old Content"
+print_head "Removing Old Files "
 rm -rf /app/* &>>${log_file}
 
-print_head "Downloading App Content"
+print_head "Downloading  app content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
-cd /app
 
-print_head "Extracting App Content"
+cd /app
+print_head "Extracting App Content "
 unzip /tmp/catalogue.zip &>>${log_file}
 
-print_head " Installing NodeJs Dependencies"
+print_head "Installaling NodeJs Dependencies "
 npm install &>>${log_file}
+status_check $?
 
-print_head "Copy SystemD Service File"
+print_head "copy SystemD service file "
 cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
 
-print_head "Reload SystemD"
+print_head "reloading SystemD "
 systemctl daemon-reload &>>${log_file}
 
-print_head "Enable Catalogue Service"
+print_head "enable catalogue Service"
 systemctl enable catalogue &>>${log_file}
 
-print_head "Start Catalogue Service"
-systemctl restart catalogue &>>${log_file}
+print_head "start catalogue service "
+systemctl start catalogue &>>${log_file}
 
-print_head "Copy MongoDB Repo File"
-cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+print_head "copy Mongodb Repo file "
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
-print_head "Install Mongo Client"
+print_head "Installaling mongo Client "
 yum install mongodb-org-shell -y &>>${log_file}
 
-print_head "Load Schema"
+print_head "Loading mongodb schema  "
 mongo --host mongodb.d-b-7.online </app/schema/catalogue.js &>>${log_file}
