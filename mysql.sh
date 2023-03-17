87e8886aa323534dd43b38754214dff289b64021
aravnanda2019 <aravnanda2019@gmail.com>
2023-03-17 21:31:58 +0530

    updating ./roboshop-shell

--- mysql.sh
+++ mysql.sh
@@ -1,27 +1,36 @@
 source common.sh
 
+mysql_root_password=$1
+
 if [ -z "${mysql_root_password}" ]; then
-  echo -e "\e[31mMissing MySql Root Password Argument\e[0m"
+  echo -e "\e[31mMissing MySQL Root Password argument\e[0m"
   exit 1
 fi
 
-mysql_root_password=$1
 print_head "Disabling MySQL 8 Version"
-dnf module disable mysql -y &>>${log_file}
+dnf module disable mysql -y  &>>${log_file}
 status_check $?
 
-print_head "Installing MySql Server"
-yum install mysql-community-server -y &>>${log_file}
+print_head "Copy MySQL repo file"
+cp ${code_dir}/configs/mysql.repo  /etc/yum.repos.d/mysql.repo  &>>${log_file}
 status_check $?
 
-print_head "Enable MySql Service"
-systemctl enable mysqld &>>${log_file}
+print_head "Installing MySQL Server"
+yum install mysql-community-server -y  &>>${log_file}
+status_check $?
+
+print_head "Enable MYSQL Service"
+systemctl enable mysqld  &>>${log_file}
 status_check $?
 
 print_head "Start MySQL Service"
-systemctl start mysqld &>>${log_file}
+systemctl start mysqld  &>>${log_file}
 status_check $?
 
 print_head "Set Root Password"
-mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+
+echo show databases | mysql -uroot -p${mysql_root_password} &>>${log_file}
+if [ $? -ne 0 ]; then
+  mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>${log_file}
+fi
 status_check $?
